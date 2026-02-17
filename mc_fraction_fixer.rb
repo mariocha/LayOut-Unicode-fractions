0debdb1652da76d4bf2dba4b974dc7208f460548
Mario Chabot <marchab@me.com>
2026-02-17 16:16:46 -0500

    Fix creator name to Mario Chabot

---
+++ mc_fraction_fixer.rb
@@ -0,0 +1,29 @@
+# mc_fraction_fixer.rb
+# Fraction Fixer - Convertit les fractions LayOut en Unicode
+# Par Mario Chabot - 2026
+# Version 1.0.0
+
+require 'sketchup.rb'
+require 'extensions.rb'
+
+module MarioChacet
+  module FractionFixer
+
+    unless file_loaded?(__FILE__)
+      ex = SketchupExtension.new(
+        'Fraction Fixer',
+        'mc_fraction_fixer/main.rb'
+      )
+
+      ex.description = 'Convertit les fractions (1/2, 3/4, etc.) en caractères Unicode (½, ¾, etc.) dans les fichiers LayOut.'
+      ex.version     = '1.0.0'
+      ex.copyright   = '2026 Mario Chabot'
+      ex.creator     = 'Mario Chabot'
+
+      Sketchup.register_extension(ex, true)
+
+      file_loaded(__FILE__)
+    end
+
+  end
+end
